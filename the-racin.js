45774f0550285666b3ed844d6f2995e3dd0cbcca
tildesarecool <tildesare.cool@gmail.com>
2019-02-03 14:33:16 -0800

    sect 11.86: split some common grfx functions into separate file

--- the-racin.js
+++ the-racin.js
@@ -272,31 +272,3 @@ function drawAll() {
 
     drawTracks();
 }
-
-function drawBitmapCenteredWithRotation(useBitmap, atX, atY, withAng) {
-    // section 8.69: car spinning in place
-    // this wasn't explained in detail, only the basics
-    canvasContext.save();
-    canvasContext.translate(atX, atY);
-    canvasContext.rotate(carAng); // rotate on center of image
-    canvasContext.drawImage(useBitmap, -useBitmap.width / 2, -useBitmap.height / 2); //center image relative to self
-    canvasContext.restore(); // forgets things since last save
-
-}
-
-function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
-    canvasContext.fillStyle= fillColor;
-    canvasContext.fillRect(topLeftX,topLeftY, boxWidth,boxHeight);
-}
-
-function colorCircle(centerX,centerY, radius, fillColor) {
-    canvasContext.fillStyle = fillColor;
-    canvasContext.beginPath();
-    canvasContext.arc(centerX,centerY, radius, 0, Math.PI*2, true);
-    canvasContext.fill();
-}
-
-function colorText(showWords, textX,textY, fillColor) {
-	canvasContext.fillStyle = fillColor;
-	canvasContext.fillText(showWords, textX, textY);
-}
