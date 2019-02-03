396be69b7606ab0d916e24ed76f6e86043a58ea3
tildesarecool <tildesare.cool@gmail.com>
2019-02-03 15:20:32 -0800

    sect 11.87: logically split different functions into different files

---
+++ car.js
@@ -0,0 +1,83 @@
+// code for player1car.png - section 7.66
+// start section 9: 1/28/2019
+var carPic = document.createElement("img");
+var carPicLoaded = false;
+
+var carX = 75;
+var carY = 75;
+var carAng = 0;
+var carSpeed = 0;
+
+const GROUNDSPEED_DECAY_MULT = 0.94;
+const DRIVE_POWER = 0.5;
+const REVERSE_POWER = 0.2;
+const TURN_RATE = 0.03;
+
+function carImageLoad() {
+    carPic.onload = function() {
+        carPicLoaded = true;
+    }
+    carPic.src = "img/player1car.png"
+}
+
+function carReset() {
+	for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
+		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
+			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
+			if(trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
+                trackGrid[arrayIndex] = TRACK_ROAD;
+                carAng = -Math.PI / 2;
+				carX = eachCol * TRACK_W + TRACK_W/2;
+				carY = eachRow * TRACK_H + TRACK_H/2;
+			}
+		}
+	}
+}
+
+
+function carMove() {
+
+    carSpeed *= GROUNDSPEED_DECAY_MULT;
+
+    if (keyHeld_Gas) {
+        carSpeed += DRIVE_POWER;
+    }
+    if (keyHeld_Reverse) {
+        carSpeed -= REVERSE_POWER;
+    }
+
+    if (keyHeld_TurnLeft) {
+        carAng -= TURN_RATE;
+    }
+
+    if (keyHeld_TurnRight) {
+        carAng += TURN_RATE;
+    }
+
+
+    // sect 8.71 and 72: something something trigonometry?
+    carX += Math.cos(carAng) * carSpeed;
+    carY += Math.sin(carAng) * carSpeed;
+
+/*
+const GROUNDSPEED_DECAY_MULT = 0.94;
+const DRIVE_POWER = 0.5;
+const REVERSE_POWER = 0.2;
+const TURN_RATE = 0.03;
+*/
+
+}
+
+function carDraw() {
+    if (carPicLoaded) {
+        drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);
+    }
+
+
+}
+/*
+carPic.onload = function() {
+    carPicLoaded = true;
+}
+carPic.src = "img/player1car.png";
+*/
---
+++ track.js
@@ -0,0 +1,84 @@
+// 1/20/2019
+// section 4
+// since re-factoring is happening I split into separate file
+// which git is upposed to make unnecessary but i did it anyway
+
+const TRACK_W = 40;
+const TRACK_H = 40; 
+const TRACK_GAP = 2;
+const TRACK_COLS = 20;
+const TRACK_ROWS = 15; 
+
+
+// 1 and 0s instead of true/false
+// coulse also use other numbers for different assets like flags or trees
+// so this is a semi-visual way to layout the track
+var trackGrid = 
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1,
+    1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
+    1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+    1, 0, 2, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+    1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+    const TRACK_ROAD = 0;
+    const TRACK_WALL = 1;
+    const TRACK_PLAYERSTART = 2;
+
+    function isWallAtColRow(col, row) {
+        if (col >= 0 && col < TRACK_COLS &&
+            row >= 0 && row < TRACK_ROWS) {
+    
+            var trackIndexUnderCoord = rowColToArrayIndex(col, row);
+            return (trackGrid[trackIndexUnderCoord] == TRACK_WALL); 
+            
+        } else {
+            return false;
+        }
+    }
+
+    function carTrackHandling() {
+        var carTrackCol = Math.floor(carX / TRACK_W);
+        var carTrackRow = Math.floor(carY / TRACK_H);
+        var trackIndexUnderCar = rowColToArrayIndex(carTrackCol, carTrackRow);
+      
+       if (carTrackCol >= 0 && carTrackCol < TRACK_COLS &&
+           carTrackRow >= 0 && carTrackRow < TRACK_ROWS) {
+    
+            if (isWallAtColRow(carTrackCol, carTrackRow)) {
+                carX -= Math.cos(carAng) * carSpeed;
+                carY -= Math.sin(carAng) * carSpeed;
+                
+                carSpeed *= -0.5; 
+    
+            } // end of track found
+       } // end of valid col and row
+    
+    } // end of carTrackHandling function
+
+function rowColToArrayIndex(col, row) {
+        return col + TRACK_COLS * row;
+    }
+
+function drawTracks() {
+    for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
+        for ( var eachCol = 0; eachCol < TRACK_COLS; eachCol++ ) {
+    
+            var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+    
+            if ( trackGrid[arrayIndex] == TRACK_WALL ) { 
+                colorRect(TRACK_W * eachCol,TRACK_H*eachRow, TRACK_W-TRACK_GAP,TRACK_H - TRACK_GAP, 'blue');
+            }
+        }
+    }
+}
+    
