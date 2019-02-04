9b0058b5ef9d3f8bf8c52abc34c8a5911d63681b
tildesarecool <tildesare.cool@gmail.com>
2019-02-03 19:15:14 -0800

    Finish of sect 13.100: loading screen/text message

--- the-racin.js
+++ the-racin.js
@@ -2,7 +2,10 @@ var canvas, canvasContext;
 
 window.onload = function() {
 	canvas = document.getElementById('gameCanvas');
-	canvasContext = canvas.getContext('2d');
+    canvasContext = canvas.getContext('2d');
+    
+    colorRect(0,0, canvas.width,canvas.height, 'black'); // shows as loading, could display message/loading/whatever
+    colorText("LOADING IMAGES...", canvas.width/2, canvas.height/2, 'white');
 
     loadImages();
 }
