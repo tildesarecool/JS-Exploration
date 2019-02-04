bc3b8cb9f7c85f85da4ddae5db8137d5f1b4513b
tildesarecool <tildesare.cool@gmail.com>
2019-02-03 19:08:27 -0800

    sect 13.99 completed

--- car.js
+++ car.js
@@ -10,7 +10,7 @@ var carSpeed = 0;
 const GROUNDSPEED_DECAY_MULT = 0.94;
 const DRIVE_POWER = 0.5;
 const REVERSE_POWER = 0.2;
-const TURN_RATE = 0.03;
+const TURN_RATE = 0.05;
 
 
 
--- imageloading.js
+++ imageloading.js
@@ -1,26 +1,37 @@
 var carPic = document.createElement("img");
-//var carPicLoaded = false;
 var roadPic = document.createElement("img");
 var wallPic = document.createElement("img");
 
-var picsToLoad = 3; // 3 images to load above
+var picsToLoad = 0; // set automatically based on imageList in loadImages()
 
 function countLoadedImagesAndLaunchIfReady() {
-    picsToLoad--;
-    console.log("value of picsToLoad is "+picsToLoad);
-    if (picsToLoad == 0) {
-        imageLoadingDoneSoStartGame();
-    }
-
+	picsToLoad--;
+	console.log(picsToLoad);
+	if(picsToLoad == 0) {
+		imageLoadingDoneSoStartGame();
+	}
 }
 
 function beginLoadingImage(imgVar, fileName) {
-    imgVar.onload = countLoadedImagesAndLaunchIfReady;
-    imgVar.src = fileName;
+	imgVar.onload = countLoadedImagesAndLaunchIfReady;
+	imgVar.src = fileName;
 }
 
 function loadImages() {
-    beginLoadingImage(carPic, "img/player1car.png");
-    beginLoadingImage(roadPic, "img/track_road.png")
-    beginLoadingImage(wallPic, "img/track_wall.png");
+	// next line is just an example, not using, will remove later
+	var dataSet = {varName: carPic, theFile: "img/player1car.png"};
+
+	var imageList = [
+		{varName: carPic, theFile: "img/player1car.png"},
+		{varName: roadPic, theFile: "img/track_road.png"},
+		{varName: wallPic, theFile: "img/track_wall.png"}
+		];
+
+	picsToLoad = imageList.length;
+
+	for(var i=0;i<imageList.length;i++) {
+		beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+	}
 }
+
+
