4dca4ac8288de7f264ca8fb057db915134cc667b
tildesarecool <tildesare.cool@gmail.com>
2019-02-04 20:28:31 -0800

    14.107: refactored track code a bit

--- js/track.js
+++ js/track.js
@@ -7,10 +7,11 @@
 
 
 const TRACK_W = 40;
-const TRACK_H = 40; 
+const TRACK_H = 40;
 const TRACK_GAP = 2;
 const TRACK_COLS = 20;
-const TRACK_ROWS = 15; 
+const TRACK_ROWS = 15;
+
 
 
 
@@ -34,26 +35,25 @@ var trackGrid =
     1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-    const TRACK_ROAD = 0;
-    const TRACK_WALL = 1;
-    const TRACK_PLAYERSTART = 2;
-    const TRACK_GOAL = 3;
-    const TRACK_TREE = 4;
-    const TRACK_FLAG = 5;
+const TRACK_ROAD = 0;
+const TRACK_WALL = 1;
+const TRACK_PLAYERSTART = 2;
+const TRACK_GOAL = 3;
+const TRACK_TREE = 4;
+const TRACK_FLAG = 5;
 
 
+function isObstacleAtColRow(col, row) {
+	if(col >= 0 && col < TRACK_COLS &&
+		row >= 0 && row < TRACK_ROWS) {
+		 var trackIndexUnderCoord = rowColToArrayIndex(col, row);
+		 return (trackGrid[trackIndexUnderCoord] != TRACK_ROAD);
+	} else {
+		return false;
+	}
+}
 
-    function isWallAtColRow(col, row) {
-        if (col >= 0 && col < TRACK_COLS &&
-            row >= 0 && row < TRACK_ROWS) {
-    
-            var trackIndexUnderCoord = rowColToArrayIndex(col, row);
-            return (trackGrid[trackIndexUnderCoord] == TRACK_WALL); 
-            
-        } else {
-            return false;
-        }
-    }
+
 
     function carTrackHandling() {
         var carTrackCol = Math.floor(carX / TRACK_W);
@@ -63,7 +63,7 @@ var trackGrid =
        if (carTrackCol >= 0 && carTrackCol < TRACK_COLS &&
            carTrackRow >= 0 && carTrackRow < TRACK_ROWS) {
     
-            if (isWallAtColRow(carTrackCol, carTrackRow)) {
+            if (isObstacleAtColRow(carTrackCol, carTrackRow)) {
                 carX -= Math.cos(carAng) * carSpeed;
                 carY -= Math.sin(carAng) * carSpeed;
                 
@@ -83,13 +83,36 @@ function drawTracks() {
         for ( var eachCol = 0; eachCol < TRACK_COLS; eachCol++ ) {
     
             var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
-    
-            if ( trackGrid[arrayIndex] == TRACK_ROAD ) { 
-                canvasContext.drawImage(roadPic, TRACK_W * eachCol,TRACK_H*eachRow); 
-            } else if (trackGrid[arrayIndex] == TRACK_WALL) {
-                canvasContext.drawImage(wallPic,TRACK_W * eachCol, TRACK_H * eachRow);
+            var tileKindHere = trackGrid[arrayIndex];
+            var useImg;
+
+            switch(tileKindHere) {
+                case TRACK_ROAD:
+                    useImg = roadPic;
+                    break;
+                case TRACK_WALL:
+                    useImg = wallPic;
+                    break;
+                case TRACK_GOAL:
+                    useImg = goalPic;
+                    break;
+                case TRACK_TREE:
+                    useImg = treePic;
+                    break;
+                case TRACK_FLAG:
+                    useImg = flagPic;
+                    break;
             }
+/*
+    const TRACK_GOAL = 3;
+    const TRACK_TREE = 4;
+    const TRACK_FLAG = 5;
+
+*/
+
+            canvasContext.drawImage(useImg, TRACK_W * eachCol,TRACK_H*eachRow); 
         }
     }
 }
     
+
