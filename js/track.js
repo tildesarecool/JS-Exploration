af178bbb0d540ecc9a6eec210493d6c0070311e7
tildesarecool <tildesare.cool@gmail.com>
2019-02-05 21:54:32 -0800

    sect 15.112: optimization of nested for loops in track.js

--- js/track.js
+++ js/track.js
@@ -80,16 +80,26 @@ function rowColToArrayIndex(col, row) {
     }
 
 function drawTracks() {
+
+    var arrayIndex = 0;
+    var drawTileX = 0;
+    var drawTileY = 0;
     for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
         for ( var eachCol = 0; eachCol < TRACK_COLS; eachCol++ ) {
     
-            var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+            
             var tileKindHere = trackGrid[arrayIndex];
             var useImg = trackPics[tileKindHere]; // one line replaces switch statement
+            canvasContext.drawImage(useImg, drawTileX,drawTileY); 
 
-            canvasContext.drawImage(useImg, TRACK_W * eachCol,TRACK_H*eachRow); 
-        }
-    }
+            drawTileX += TRACK_W;
+
+
+            arrayIndex++;
+        } // end of each col
+        drawTileY += TRACK_H;
+        drawTileX = 0;
+    } // end of each row
 }
     
 
