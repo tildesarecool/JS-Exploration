ae90b917fac977f13f49c2aa43f87e0380fa4d88
Keith <tildesare.cool@gmail.com>
2019-02-07 20:37:52 -0800

    sect 16.115: Re-did car.js to be a single class rather than fucntions

--- js/car.js
+++ js/car.js
@@ -1,77 +1,65 @@
 // code for player1car.png - section 7.66
 // start section 9: 1/28/2019
 
-
-var carX = 75;
-var carY = 75;
-var carAng = 0;
-var carSpeed = 0;
-
 const GROUNDSPEED_DECAY_MULT = 0.94;
 const DRIVE_POWER = 0.5;
 const REVERSE_POWER = 0.2;
 const TURN_RATE = 0.05;
 const MIN_SPEED_TO_TURN = 0.5;
 
+function carClass() {
+// sect 16.115: converting to class
+    this.x = 75;
+    this.y = 75;
+    this.ang = 0;
+    this.speed = 0;
 
 
-function carReset() {
-	for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
-		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
-			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
-			if(trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
-                trackGrid[arrayIndex] = TRACK_ROAD;
-                carAng = -Math.PI / 2;
-				carX = eachCol * TRACK_W + TRACK_W/2;
-				carY = eachRow * TRACK_H + TRACK_H/2;
-			}
-		}
-	}
-}
 
 
-function carMove() {
+    this.reset = function() {
 
-    carSpeed *= GROUNDSPEED_DECAY_MULT;
+        for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
+            for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
+                var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
+                if(trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
+                    trackGrid[arrayIndex] = TRACK_ROAD;
+                    carAng = -Math.PI / 2;
+                    carX = eachCol * TRACK_W + TRACK_W/2;
+                    carY = eachRow * TRACK_H + TRACK_H/2;
+                } //end of playert start if
+            } // end of col for 
+        } // end of row for
+    } // end of carReset func
 
-    if (keyHeld_Gas) {
-        carSpeed += DRIVE_POWER;
-    }
-    if (keyHeld_Reverse) {
-        carSpeed -= REVERSE_POWER;
-    }
 
-    if (Math.abs(carSpeed) > MIN_SPEED_TO_TURN ) {
+    this.move = function  () {
 
-        if (keyHeld_TurnLeft) {
-            carAng -= TURN_RATE;
+        carSpeed *= GROUNDSPEED_DECAY_MULT;
+
+        if (keyHeld_Gas) {
+            carSpeed += DRIVE_POWER;
+        }
+        if (keyHeld_Reverse) {
+            carSpeed -= REVERSE_POWER;
         }
 
-        if (keyHeld_TurnRight) {
-            carAng += TURN_RATE;
+        if (Math.abs(carSpeed) > MIN_SPEED_TO_TURN ) {
+
+            if (keyHeld_TurnLeft) {
+                carAng -= TURN_RATE;
+            }
+
+            if (keyHeld_TurnRight) {
+                carAng += TURN_RATE;
+            }
         }
-    }
-
-
     // sect 8.71 and 72: something something trigonometry?
-    carX += Math.cos(carAng) * carSpeed;
-    carY += Math.sin(carAng) * carSpeed;
+        carX += Math.cos(carAng) * carSpeed;
+        carY += Math.sin(carAng) * carSpeed;
+    }
 
-/*
-const GROUNDSPEED_DECAY_MULT = 0.94;
-const DRIVE_POWER = 0.5;
-const REVERSE_POWER = 0.2;
-const TURN_RATE = 0.03;
-*/
-
+    this.draw = function() {
+        drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);
+    }
 }
-
-function carDraw() {
-    drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);
-}
-/*
-carPic.onload = function() {
-    carPicLoaded = true;
-}
-carPic.src = "img/player1car.png";
-*/
